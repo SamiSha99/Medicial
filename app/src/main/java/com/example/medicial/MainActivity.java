27565657111ff5a1fb2b901a72fd20b2683f6827
Obada daraghmeh <obada.e.amer@gmail.com>
2022-11-28 12:08:40 +0200

    Edit layouts design and can now move between MainActivity ,LoginActivity.

--- app/src/main/java/com/example/medicial/MainActivity.java
+++ app/src/main/java/com/example/medicial/MainActivity.java
@@ -5,17 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_Call_Login_Activity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_Call_Login_Activity = findViewById(R.id.btn_start);
+        btn_Call_Login_Activity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Call_Login_Activity(view);
+            }
+
+            public  void Call_Login_Activity(View view){
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+
+        });
     }
 
-    public void Call_Login_Activity(View v){
-        Intent intent = new Intent(this , LoginActivity.class);
-        startActivity(intent);
-    }
+
 }
