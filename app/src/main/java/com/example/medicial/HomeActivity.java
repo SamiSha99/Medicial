22e5c8999e76355598acb0bbcf5e8e22f085754a
Obada daraghmeh <obada.e.amer@gmail.com>
2022-12-22 18:53:33 +0200

    Modified

--- app/src/main/java/com/example/medicial/HomeActivity.java
+++ app/src/main/java/com/example/medicial/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.medicial;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
