ac067d1c36e357e71546d05a7b3ed0df70980644
SamiSha <Sami.shakkour@hotmail.com>
2023-01-06 17:10:13 +0200

    Unnested Register(), Toast for "empty fields".

--- app/src/main/java/com/example/medicial/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/medicial/Activity/RegisterActivity.java
@@ -26,11 +26,11 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-//        {Full screen activity}
+        // {Full screen activity}
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-//        {Hook Edittext fields}
+        // {Hook Edittext fields}
         username = findViewById(R.id.edt_reg_userName);
         firstName = findViewById(R.id.edt_reg_firstName);
         lastName = findViewById(R.id.edt_reg_lastName);
@@ -43,49 +43,84 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void Register() {
-        String Username = username.getText().toString();
-        String Firstname = firstName.getText().toString();
-        String Lastname = lastName.getText().toString();
-        String Email = email.getText().toString();
-        String Password = password.getText().toString();
-        String Re_password = re_password.getText().toString();
-
-        // To check if fields are empty or not
-        if (TextUtils.isEmpty(Username) || (TextUtils.isEmpty(Firstname)) || (TextUtils.isEmpty(Lastname)) || (TextUtils.isEmpty(Email)) || (TextUtils.isEmpty(Password)) || (TextUtils.isEmpty(Re_password))) {
+        String _Username = username.getText().toString();
+        String _Firstname = firstName.getText().toString();
+        String _Lastname = lastName.getText().toString();
+        String _Email = email.getText().toString();
+        String _Password = password.getText().toString();
+        String _Re_password = re_password.getText().toString();
+        boolean emptyField = false;
+        if(TextUtils.isEmpty(_Username))
+        {
             username.setHint("required field");
             username.setHintTextColor(getResources().getColor(R.color.red));
+            emptyField = true;
+        }
 
+        if(TextUtils.isEmpty(_Firstname))
+        {
             firstName.setHint("required field");
             firstName.setHintTextColor(getResources().getColor(R.color.red));
+            emptyField = true;
+        }
 
+        if(TextUtils.isEmpty(_Lastname))
+        {
             lastName.setHint("required field");
             lastName.setHintTextColor(getResources().getColor(R.color.red));
+            emptyField = true;
+        }
 
+        if(TextUtils.isEmpty(_Email))
+        {
             email.setHint("required field");
             email.setHintTextColor(getResources().getColor(R.color.red));
+            emptyField = true;
+        }
 
+        if(TextUtils.isEmpty(_Password))
+        {
             password.setHint("required field");
             password.setHintTextColor(getResources().getColor(R.color.red));
+            emptyField = true;
+        }
 
+        if(TextUtils.isEmpty(_Re_password))
+        {
             re_password.setHint("required field");
             re_password.setHintTextColor(getResources().getColor(R.color.red));
-        } else {
-            if (Password.equals(Re_password)) {
-                boolean check_user_name = dbHelper.CheckUserName(Username);
-                if (!check_user_name) { //check_user_name == false
-                    boolean insert_User_Data = dbHelper.insertUserData(Username, Firstname, Lastname, Password, Email);
-                    if (insert_User_Data) { //insert_User_Data == true
-                        replaceFragment(new SuccessFragment());
-                    } else {
-                        Toast.makeText(RegisterActivity.this, "REGISTRATION FAILED", Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(RegisterActivity.this, "User already exists!", Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Toast.makeText(RegisterActivity.this, "Password not matching", Toast.LENGTH_SHORT).show();
-            }
+            emptyField = true;
         }
+
+        // Empty Fields
+        if(emptyField)
+        {
+            Toast.makeText(this, "Some fields are empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Password verification mismatch
+        if(!_Password.equals(_Re_password))
+        {
+            Toast.makeText(this, "Password not matching", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Username already exists
+        if(dbHelper.CheckUserName(_Username))
+        {
+            Toast.makeText(this, "User already exists!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // DB insertion failure
+        if(!dbHelper.insertUserData(_Username, _Firstname, _Lastname, _Password, _Email))
+        {
+            Toast.makeText(this, "REGISTRATION FAILED", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        replaceFragment(new SuccessFragment());
     }
 
     private void replaceFragment(Fragment fragment) {
